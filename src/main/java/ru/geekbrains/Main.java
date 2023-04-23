f788a82d78eaf6e4656f67cd79e7d1ed95d07993
Sergey <elec84@mail.ru>
2023-04-23 23:29:20 +0300

    lesson1

---
+++ src/main/java/ru/geekbrains/Main.java
@@ -0,0 +1,18 @@
+package ru.geekbrains;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = Person.builder()
+                .firstName("Ivan")
+                .lastName("Petrov")
+                .middleName("Ivanov")
+                .country("Russia")
+                .address("Aleksandrov")
+                .phone("89997776655")
+                .age(30)
+                .gender("male")
+                .build();
+
+        System.out.println(person1);
+    }
+}
